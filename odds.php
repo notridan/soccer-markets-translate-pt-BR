5eb8e9c3ec66f57aa060513eedf46fa748b31c8f
Paulo Henrique Nascimento da Silva (Notridan) <paulo@notridan.com>
2018-06-04 10:07:01 -0300

    Some odds translateds

--- odds.php
+++ odds.php
@@ -8,6 +8,32 @@
 
 return [
 
+	'1' => 'Casa',
+	'2' => 'Visitante',
+	'X' => 'Empate',
+	'Under' => 'Abaixo de ',
+	'Over' => 'Acima de',
+	'No' =>	'Não',
+	'Yes' => 'Sim',
+	'Home/Draw' => 'Casa/Empate',
+	'Home/Away' => 'Casa/Visitante',
+	'Draw/Away' => 'Empate/Visitante',
+	'Draw/Draw' => 'Empate/Empate',
+	'1st Half' => '1º Tempo',
+	'2st Half' => '2º Tempo',
+	'more 7' => 'Acima de 7',
+	'Home/Yes' => 'Casa/Sim',
+	'Draw/Yes' => 'Empate/Sim',
+	'Away/Yes' => 'Visitante/Sim',
+	'Home/No' => 'Casa/Não',
+	'Draw/No' => 'Empate/Não',
+	'Away/No' => 'Visitante/Não',
+	'Home/Over' => 'Casa / Acima de',
+	'Draw/Over' => 'Empate / Acima de',
+	'Away/Over' => 'Visitante / Acima de',
+	'Home/Under' => 'Casa / Abaixo de',
+	'Draw/Under' => 'Empate / Abaixo de',
+	'Away/Under' => 'Visitante / Abaixo de',
 
 
 ];
